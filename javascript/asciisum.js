0a02bf55d651c529913c8eaf173a24d9a3a6ed6c
lampridiforme <macroavis@gmail.com>
2016-06-12 12:34:28 -0700

    Improved ASCII Implementation
    Added the checkboxes and included the implementation

--- javascript/asciisum.js
+++ javascript/asciisum.js
@@ -1,40 +1,30 @@
-oCheckBox1 = oForm.elements["spaces"];
-oCheckBox3 = oForm.elements["lower"]
-oCheckBox2 = oForm.elements["upper"];
-
-var spacesOn = false;
-var lowercaseOnly = false;
-var uppercaseOnly = false;
-
-function checkTest(oCheckbox, bool){
-	var checkVal = oCheckbox.value;
-	if(oCheckbox.checked){
-		bool = true;
-	}
-}
-
 function sum() {
 	var sentence = document.getElementById("sentence").value;	//Get the string	
 	var length = sentence.length;	//Get the length of the string
 	var currentValue = 0;
 	var sum = 0;	//The sum of the character	
 	
-//	checkTest(oCheckBox1, spacesOn);
-//	checkTest(oCheckBox2, lowercaseOnly);
-//	checkTest(oCheckBox3, uppercaseOnly);
+	var spaceCheck = document.getElementById("spaces");
+	var upperCheck = document.getElementById("upper");
+	var lowerCheck = document.getElementById("lower");
 	
 	for(var n=0; n<length; n++) {	//Increment through the length to access all chars
 	
-		if( lowercaseOnly ){	//Convert to lowercase
+		if( lowerCheck.checked && upperCheck.checked ){
+			document.getElementById("result").innerHTML = "Please only select either lower or upper case.";
+			return;
+		}
+	
+		if( lowerCheck.checked ){	//Convert to lowercase
 			sentence = sentence.toLowerCase();
 		}
-		else if( uppercaseOnly ){
+		else if( upperCheck.checked ){
 			sentence = sentence.toUpperCase();
 		}
 	
 		currentValue = sentence.charCodeAt(n);	//Calculate the code
 		
-		if( (currentValue == 32) && spacesOn ){	//Do not take the sum if space when spaces are not allowed
+		if( (currentValue == 32) && spaceCheck.checked ){	//Do not take the sum if space when spaces are not allowed
 			continue;
 		}
 		
