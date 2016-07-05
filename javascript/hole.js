bd350ec66bea8da95fba03d50919dd893d5b4251
lampridiforme <macroavis@gmail.com>
2016-07-04 23:13:04 -0700

    Pushing for John...
    Button works and can increment a bit

--- javascript/hole.js
+++ javascript/hole.js
@@ -1,20 +1,44 @@
 //Number of clicks on the button
-clicks = 0;
- 
-dig(){
+var clicks = 0;
+//Width of the hole; effects how many things you can keep in your hole
+var width = 0;
+//Saves inventory contents string for tools
+var tools = "";
+//Same for materials
+var materials = "";
+//Same for treasure
+var treasure = "";
+  
+/* dig(): increments the click counter */
+function dig(){
 	clicks++;
-	updateDepth(clicks);
-	
+	if(clicks == 1){
+		document.getElementById("digButton").innerHTML = "Dig";
+	}
+	else if(clicks > 1){
+		updateDepth(clicks-1);	//Subtract 1 so that there isn't one extra
+								//meter, but theres got to be a better way...
+		if(clicks == 2){
+			width = 1;
+			updateWidth(width);
+		}
+	}
  }
  
-updateDepth(value){
+/* updateDepth(int): updates the HTML depth line to equal the new value */
+function updateDepth(value){
+	document.getElementById("depth").innerHTML = "Depth: " + value + "m";
+}
+
+/* updateWidth(int): updates the HTML radius line to equal the new value */
+function updateWidth(value){
+	document.getElementById("width").innerHTML = "Radius: " + value + "m";
+}
+
+function generateLayerName(){
 	
 }
 
-generateLayerName(){
-	
-}
-
-generateBiomeName(){
+function generateBiomeName(){
 	
 }
