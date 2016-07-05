17585ff40dc7560bc7a7086d69d0678e3a9a7033
lampridiforme <macroavis@gmail.com>
2016-07-04 20:10:46 -0700

    Digging Simulator, Contact image
    Contact image changed to a dicehead.
    
    Digging simulator layout almost done.

---
+++ javascript/hole.js
@@ -0,0 +1,20 @@
+//Number of clicks on the button
+clicks = 0;
+ 
+dig(){
+	clicks++;
+	updateDepth(clicks);
+	
+ }
+ 
+updateDepth(value){
+	
+}
+
+generateLayerName(){
+	
+}
+
+generateBiomeName(){
+	
+}
