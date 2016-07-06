12d8c8cb1fed2abfc1d9f38fa898a8f7dc67812c
lampridiforme <macroavis@gmail.com>
2016-07-06 13:51:51 -0700

    vsdfvsfdvsdsd

--- Javascript/timer.js
+++ Javascript/timer.js
@@ -4,7 +4,7 @@
 var myVar = setInterval(myTimer, 10000); // Delay 10 seconds
 
 function myTimer() {
-    document.getElementById("demo").innerHTML = "Hello World and all those who inhabit it!"; //Changes demo's text to "hello world..."
+    document.getElementById("demo").innerHTML = "Your Neopets are hungry!"; //Changes demo's text to "hello world..."
 }
 
 // Count the seconds
@@ -16,14 +16,14 @@ function counter() {
 	counterVal++;
 	document.getElementById("counter").innerHTML = counterVal;
 }
-// Display a message every once in a while, every half second
-var weaselStart = setInterval(weasel, 500);
+// Display a message every once in a while, every (I can't do math) second
+var weaselStart = setInterval(weasel, 200);
 
 function weasel() {
 	var randomNum = Math.floor(100*Math.random());
 	var popped = 0;
 	
-	if (randomNum <= 30 && popped == 0) {
+	if (randomNum <= 5 && popped == 0) {
 		document.getElementById("weasel").innerHTML = "POP!";
 		popped = 1;
 	}
@@ -31,3 +31,25 @@ function weasel() {
 		document.getElementById("weasel").innerHTML = "<br>";
 	}
 }
+
+// Start a counter and store info
+//Call it in the beginning to avoid the initial delay
+
+counter2();
+
+var counter2Timer = setInterval(counter2, 1000);
+
+function counter2() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.counter2Num) {
+            localStorage.counter2Num = ++localStorage.counter2Num;
+        }
+		else {
+			localStorage.counter2Num = 1;
+		}
+        document.getElementById("counter2").innerHTML = "localStorage counter is at " + localStorage.counter2Num + " second(s).";
+	} else {
+		document.getElementById("counter2").innerHTML = "Sorry, your browser does not support Web Storage...";
+	}
+}
+	
